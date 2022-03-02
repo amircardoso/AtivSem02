04046cbdbe16edeb9b52fab50048ec19ff39e892
Amir TJ Cardoso <57577175+amircardoso@users.noreply.github.com>
2022-03-02 09:55:41 -0300

    Add files via upload

---
+++ AtivSem02/Biblioteca/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca;
+
+namespace Biblioteca
+{
+    public class Class1
+    {
+        List<string> Alunos = new List<string>();
+        
+        Alunos.Add("Auro");
+        Alunos.Add("Herculano");
+        Alunos.Add("Berenice");
+        Alunos.Add("Luiz Antonio");
+        Alunos.Add("Eustáquio");
+        Alunos.Add("Luzia");
+        Alunos.Add("Lucélia");
+        Alunos.Add("Carmen");
+        Alunos.Add("Geraldino");
+        Alunos.Add("Edinelza");
+                    
+        foreach (string item in Alunos)
+         {
+            Console.WriteLine(nome + " ");
+         }
+
+            Console.ReadKey();
+    }
+}
---
+++ AtivSem02/Biblioteca/InjDep.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Banco
+{
+    public abstract Banco
+
+        interface IBancos
+    {
+        void Transferir();
+        void Receber();
+    }
+
+}
